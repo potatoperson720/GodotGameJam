0581199d5b5ca9112681459856fcc72868d4da1a
Skittle <milanverh@gmail.com>
2026-05-09 13:16:24 +0200

    spider
    spider werkt en je moet ook nog wat variable aanpassen

--- git-test/Scenes/spider.gd
+++ git-test/Scenes/spider.gd
@@ -1,42 +1,56 @@
 extends Node2D
 
 @export var speed = 25.0
-
-
-#voeg nog toe |
 @export var player : CharacterBody2D
+@export var bullet_scene : PackedScene
+@export var shoot_cooldown = 2.0
 
 @onready var sprite = $AnimatedSprite2D
-@onready var PlayerCheck = $Area2D/PlayerCheck
+@onready var PlayerCheck = $Area2D2/PlayerCheck  # detectie cirkel
+
+var direction = 1
+var shoot_timer = 0.0
+var player_detected = false
 
 func _process(delta: float) -> void:
-	position.x += speed*delta*direction
-	var player_in_range = false
+	shoot_timer -= delta
 
-	if PlayerLeft.is_colliding():
-		if PlayerLeft.get_collider() is Player:
-			set_direction(-1)
-			player_in_range = true
-	if PlayerRight.is_colliding():
-		if PlayerRight.get_collider() is Player:
-			set_direction(1)
-			player_in_range = true
-			
-	if player_in_range and shoot_timer <=0:
-		shoot_at_player()
-		shoot_timer = shoot_cooldown
+	if player_detected and player != null:
+		# Loop naar de speler toe
+		var dir = sign(player.global_position.x - global_position.x)
+		set_direction(dir)
+		position.x += speed * delta * direction
 
-
-	
+		# Schiet als timer klaar is
+		if shoot_timer <= 0:
+			shoot_at_player()
+			shoot_timer = shoot_cooldown
+	else:
+		# Gewoon blijven staan als speler niet in range is
+		pass
 
 func set_direction(dir):
 	direction = dir
 	sprite.flip_h = direction < 0
-	
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body is Player:
-		get_tree().reload_current_scene()
 
+func shoot_at_player():
+	if bullet_scene and player:
+		var bullet = bullet_scene.instantiate()
+		get_parent().add_child(bullet)
+		bullet.global_position = global_position
+		var dir_vec = (player.global_position - global_position).normalized()
+		bullet.direction = dir_vec  # pas aan op jouw bullet script
 
+# Area2D2 = detectie cirkel (groot) → enemy loopt naar speler
 func _on_area_2d_2_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+	if body == player:
+		player_detected = true
+
+func _on_area_2d_2_body_exited(body: Node2D) -> void:
+	if body == player:
+		player_detected = false
+
+# Area2D = kill cirkel (klein) → speler sterft
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body == player:
+		get_tree().reload_current_scene()
